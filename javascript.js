785c90f1a4cf5834559f1d29717e897f74bb90f5
Frederic Feliciane <fredericfeliciane8@gmail.com>
2022-12-01 13:02:05 +0400

    some issues encountered, practice folder on pause

--- javascript.js
+++ javascript.js
@@ -1,17 +1,25 @@
+// storing the API in a varialbe
 const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
 
+// creating an array to store the API data
 const cities = []
 
+// call and response of the API, and also reading it as json format.
 const connection = fetch(url)
   .then(blob => blob.json())
   .then(data => cities.push(...data))
-// console.log(connection)
-
 
 // compare the word typed with the array
 function findMatches(wordTyped, cities) {
  const myarray = cities.filter(item => {
-  console.log(`${wordTyped} and ${item.city}`)
+  const regex = new RegExp(wordTyped, 'gi')
+  // return item.city.match(regex) || item.state.match(regex)
+  console.log('test')
  })
- return myarray
 }
+
+
+
+// displaying the result
+
+// get the dom
